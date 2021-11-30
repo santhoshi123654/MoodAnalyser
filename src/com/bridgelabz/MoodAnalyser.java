e18f5febb92bf81bf0666cba5f5c4c08b1698a29
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-30 21:00:55 +0530

    Added:Refactor Sad Mood

---
+++ src/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+public class MoodAnalyser {
+
+	String message;
+	
+	public MoodAnalyser() {
+		
+	}
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+	public String analyseMood(String message) {
+		
+		if(message.contains("Sad")) {
+			return "Sad";
+		}
+		
+		else {
+			return null;
+		}
+	}
+}
---
+++ src/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class MoodAnalyserTest {
+
+public static MoodAnalyser moodAnalyser;
+	
+	@BeforeClass
+	public static void initialize() {
+		
+		moodAnalyser = new MoodAnalyser();
+	}
+
+	@Test
+	public void analyse_the_mood_if_sad_return_Sad() {
+		
+		String message = "I am in Sad Mood";
+		String presentMood = moodAnalyser.analyseMood(message);
+		
+		assertEquals("Sad", presentMood);	
+	}
+}
