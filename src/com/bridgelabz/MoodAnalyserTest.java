aa20ae0faf965f6d3910b4657b58bf8ceac415af
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-30 20:54:52 +0530

    Added:Refactor of MoodAnalyser Happy, Sad Tests

---
+++ src/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+
+public static MoodAnalyser moodAnalyser;
+	
+	@BeforeClass
+	public static void initialize() {
+		
+		moodAnalyser = new MoodAnalyser();
+	}
+
+	@Test
+	public void analyse_the_mood_if_happy_return_Happy() {
+		
+		String message = "I am in Happy Mode";
+		String presentMood = moodAnalyser.analyseMood(message);
+		
+		assertEquals("Happy", presentMood);	
+	}
+	
+	@Test
+	public void analyse_the_mood_if_sad_return_Sad() {
+		
+		String message = "I am in Sad Mode";
+		String presentMood = moodAnalyser.analyseMood(message);
+		
+		assertEquals("Sad", presentMood);	
+	}
+}
